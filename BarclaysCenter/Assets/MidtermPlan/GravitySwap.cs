373ecbd528662ade45c7ad61ab2df09e75dcb6b7
masterdd15 <masterdd15@gmail.com>
2021-10-20 12:33:53 -0400

    Added 4 Directions to Gravity
    Made a list so gravity direction can be easily changed

--- BarclaysCenter/Assets/MidtermPlan/GravitySwap.cs
+++ BarclaysCenter/Assets/MidtermPlan/GravitySwap.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class GravitySwap : MonoBehaviour
 {
+    public enum Direction { None, Right, Left, Up, Down }
+    public Direction currentDir = Direction.None;
+
     private Rigidbody2D rb;
 
     public float gravity = -10f;
+    public Vector2 dirGravity;
     float velocity;
 
     private void Awake()
@@ -14,6 +18,7 @@ public class GravitySwap : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         //We are going to be creating our own gravity
         rb.gravityScale = 0;
+        currentDir = Direction.Down;
     }
 
     // Start is called before the first frame update
@@ -22,16 +27,35 @@ public class GravitySwap : MonoBehaviour
         
     }
 
+    private void Update()
+    {
+        gravityChanger();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(rb.mass * new Vector2(0, -gravity), ForceMode2D.Force);
+        rb.AddForce(rb.mass * dirGravity, ForceMode2D.Force);
     }
 
-
-    public void Attract(Transform body)
+    void gravityChanger()
     {
-        Vector3 gravityUp = (body.position - transform.position).normalized;
-        Vector3 bodyUp = body.up;
+        switch(currentDir)
+        {
+            case Direction.Right:
+                dirGravity = new Vector2(-gravity, 0);
+                break;
+            case Direction.Left:
+                dirGravity = new Vector2(gravity, 0);
+                break;
+            case Direction.Up:
+                dirGravity = new Vector2(0, -gravity);
+                break;
+            case Direction.Down:
+                dirGravity = new Vector2(0, gravity);
+                break;
+         
+        }
     }
+
 }
