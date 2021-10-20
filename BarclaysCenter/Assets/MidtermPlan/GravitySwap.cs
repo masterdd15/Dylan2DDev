bf923243e553bcd40db16d1a36bccd9209e0521b
masterdd15 <masterdd15@gmail.com>
2021-10-20 12:53:40 -0400

    Added Rotation
    Making object rotate with the angle change. Still have to figure out how to lock it to the angle

--- BarclaysCenter/Assets/MidtermPlan/GravitySwap.cs
+++ BarclaysCenter/Assets/MidtermPlan/GravitySwap.cs
@@ -11,6 +11,8 @@ public class GravitySwap : MonoBehaviour
 
     public float gravity = -10f;
     public Vector2 dirGravity;
+
+    public float targetRot = 0f;
     float velocity;
 
     private void Awake()
@@ -19,6 +21,7 @@ public class GravitySwap : MonoBehaviour
         //We are going to be creating our own gravity
         rb.gravityScale = 0;
         currentDir = Direction.Down;
+        targetRot = 0;
     }
 
     // Start is called before the first frame update
@@ -29,33 +32,44 @@ public class GravitySwap : MonoBehaviour
 
     private void Update()
     {
-        gravityChanger();
+        GravityChanger();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.AddForce(rb.mass * dirGravity, ForceMode2D.Force);
+        ObjectRotate();
     }
 
-    void gravityChanger()
+    void GravityChanger()
     {
         switch(currentDir)
         {
             case Direction.Right:
                 dirGravity = new Vector2(-gravity, 0);
+                targetRot = 90;
                 break;
             case Direction.Left:
                 dirGravity = new Vector2(gravity, 0);
+                targetRot = 270;
                 break;
             case Direction.Up:
                 dirGravity = new Vector2(0, -gravity);
+                targetRot = 180;
                 break;
             case Direction.Down:
                 dirGravity = new Vector2(0, gravity);
+                targetRot = 360;
                 break;
          
         }
+    }
+
+    void ObjectRotate()
+    {
+        //
+        transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, new Vector3(0, 0, targetRot), Time.deltaTime);
     }
 
 }
